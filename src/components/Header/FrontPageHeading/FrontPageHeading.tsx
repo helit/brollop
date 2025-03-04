2fe64dae7b3359a7dcf292cd84e59427acb1eed8
Henrik Littke <henrik.littke@consultant.volvo.com>
2025-03-05 00:38:08 +0100

    feat: add border

--- src/components/Header/FrontPageHeading/FrontPageHeading.tsx
+++ src/components/Header/FrontPageHeading/FrontPageHeading.tsx
@@ -7,8 +7,9 @@ export const FrontPageHeading = () => {
         {"Välkommen"}
       </Typography>
       <Typography style={{textShadow: 'rgb(0 0 0) 1px 0px 10px'}} fontFamily={"Charm"} variant="h3">
-        {"till Henke & Lottens bröllop!"}
+        {"till Henke & Lottens bröllop"}
       </Typography>
+      <div style={{backgroundColor: 'white', width: '100%', height: '3px', boxShadow: 'rgb(0 0 0) 1px 0px 10px', borderRadius: '30%'}} />
     </>
   );
 };
