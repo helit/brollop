2edb4b09831d339fc8543b8a3cccef0897a9e6c8
Henrik Littke <henrik.littke@consultant.volvo.com>
2025-03-20 21:28:25 +0100

    test changing vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/brollop',
+  base: '/brollop/',
   plugins: [react()],
   define: {
     'process.env': process.env,
