5bd25fca21a66596b92b1af02be9c9953f92ac4c
Henrik Littke <henrik.littke@consultant.volvo.com>
2025-03-20 21:58:00 +0100

    setup gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { ghPages } from 'vite-plugin-gh-pages';
 
 export default defineConfig({
   base: '/brollop/',
-  plugins: [react()],
+  plugins: [react(), ghPages()],
   define: {
     'process.env': process.env,
   },
